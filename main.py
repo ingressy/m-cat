33962e502b5e78050f2c65b5455c463b9e9046f8
ingressy <j.czinzoll@ingressy.de>
2025-01-09 17:51:08 +0100

    Add a config file an comments

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from lib import epd2in13_V4
-from PIL import Image, ImageDraw, ImageFont, ImageOps
-import os, time, socket, subprocess
+import json#,os, time, socket, subprocess
 from bootscreen import boot
+#from lib import epd2in13_V4
+#from PIL import Image, ImageDraw, ImageFont, ImageOps
 
 def main():
-    boot()
+    try:
+        #try to load the config.json file
+        with open('config.json', 'r') as configfile:
+            data = json.load(configfile)
 
+            #load the config var
+            ver = data["config"][0]["version"]
+
+            #started the boot screen with the ver var
+            boot(ver)
+
+    except FileNotFoundError:
+        #created the config.json file
+        configfile = {"config": [
+            {"version": "v0.2"}
+        ]}
+        with open('config.json', 'w') as file:
+            json.dump(configfile, file, indent=4)
 
 if __name__ == "__main__":
     main()
