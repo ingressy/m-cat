eacc0db2b624a7b1b055118f08c6d066ff0eac9e
ingressy <j.czinzoll@ingressy.de>
2025-01-13 18:47:37 +0100

    add a better schedule

--- main.py
+++ main.py
@@ -14,7 +14,25 @@ def main():
             #started the boot screen with the ver var
             boot(ver)
             dash()
-            schedule.every().hour.do(dash)
+
+            #schedule every day
+            schedule.every().day.at("00:00").do(dash)
+            schedule.every().day.at("05:00").do(dash)
+            schedule.every().day.at("06:00").do(dash)
+            schedule.every().day.at("07:00").do(dash)
+            schedule.every().day.at("08:00").do(dash)
+            schedule.every().day.at("09:00").do(dash)
+            schedule.every().day.at("10:00").do(dash)
+
+            schedule.every().day.at("12:00").do(dash)
+            schedule.every().day.at("15:00").do(dash)
+            schedule.every().day.at("17:00").do(dash)
+
+            schedule.every().day.at("19:00").do(dash)
+            schedule.every().day.at("20:00").do(dash)
+            schedule.every().day.at("21:00").do(dash)
+            schedule.every().day.at("22:00").do(dash)
+            schedule.every().day.at("23:00").do(dash)
 
         while True:
             schedule.run_pending()
@@ -23,7 +41,7 @@ def main():
     except FileNotFoundError:
         #created the config.json file
         configfile = {"config": [
-            {"version": "v0.4"}
+            {"version": "v0.5", "latitude": "0", "longitude": "0", "birthday_month": "1", "birthday_day": "1"}
         ]}
         with open('config.json', 'w') as file:
             json.dump(configfile, file, indent=4)
