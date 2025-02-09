fee1b435219ff736cd6faf7ca188dd9cabad4130
ingressy <j.czinzoll@ingressy.de>
2025-02-09 22:18:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
     except FileNotFoundError:
         #created the config.json file
         configfile = {"config": [
-            {"version": "v0.6Webuntis", "webinterface": "true",
+            {"version": "v0.6Untis", "webinterface": "true",
             "font-file": "/home/ingressy/mcat/code/Roboto-Regular.ttf"},
             {"latitude": "53.0451", "longitude": "8.8535"},
             {"birthday_month": "", "birthday_day": ""},
