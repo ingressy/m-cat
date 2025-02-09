8e620ae128f0d1df60aea4db494636277530ca87
ingressy <j.czinzoll@ingressy.de>
2025-02-09 20:10:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,32 @@
-import json, time, schedule
+import json, time, schedule, logging
 from bootscreen import boot
 from dashboard import dash
 
 def main():
     try:
         #try to load the config.json file
-        with open('config.json', 'r') as configfile:
+        with open('/home/ingressy/mcat/code/config.json', 'r') as configfile:
             data = json.load(configfile)
 
             #load the config var
             ver = data["config"][0]["version"]
+            
+            logging.basicConfig(level=logging.DEBUG)
+            logging.debug("Starting m-cat")
 
             #started the boot screen with the ver var
             boot(ver)
+
+            logging.debug("displaying bootscreen")
+
             dash()
+            logging.debug("displaying dashboard")
 
             #schedule every day
-            schedule.every().day.at("00:00").do(dash)
-            schedule.every().day.at("05:00").do(dash)
-            schedule.every().day.at("06:00").do(dash)
-            schedule.every().day.at("07:00").do(dash)
-            schedule.every().day.at("08:00").do(dash)
-            schedule.every().day.at("09:00").do(dash)
-            schedule.every().day.at("10:00").do(dash)
-
-            schedule.every().day.at("12:00").do(dash)
-            schedule.every().day.at("15:00").do(dash)
-            schedule.every().day.at("17:00").do(dash)
-
-            schedule.every().day.at("19:00").do(dash)
-            schedule.every().day.at("20:00").do(dash)
-            schedule.every().day.at("21:00").do(dash)
-            schedule.every().day.at("22:00").do(dash)
-            schedule.every().day.at("23:00").do(dash)
+            schedule.every().hour.at(":00").do(dash)
+            schedule.every().hour.at(":15").do(dash)
+            schedule.every().hour.at(":30").do(dash)
+            schedule.every().hour.at(":45").do(dash)
 
         while True:
             schedule.run_pending()
@@ -41,9 +35,15 @@ def main():
     except FileNotFoundError:
         #created the config.json file
         configfile = {"config": [
-            {"version": "v0.5", "latitude": "0", "longitude": "0", "birthday_month": "1", "birthday_day": "1"}
+            {"version": "v0.6Webuntis", "webinterface": "true",
+            "font-file": "/home/ingressy/mcat/code/Roboto-Regular.ttf"},
+            {"latitude": "53.0451", "longitude": "8.8535"},
+            {"birthday_month": "", "birthday_day": ""},
+            {"untisenable": "false", "server": "", "username": "",
+             "password": "", "school": "",
+             "useragent": "", "class": "}
         ]}
-        with open('config.json', 'w') as file:
+        with open('/home/ingressy/mcat/code/config.json', 'w') as file:
             json.dump(configfile, file, indent=4)
 
 if __name__ == "__main__":
